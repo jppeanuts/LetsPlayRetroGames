4914816d1076dffc3cdd7278c335f45a1e8f9928
jp_peanuts <stamura0626g@gmail.com>
2020-11-20 06:09:43 +0900

    Add files via upload

---
+++ HelloWorld/03_Build_and_Run.md
@@ -0,0 +1,8 @@
+## ビルドと実行
+・画面左側「HelloWorld」プロジェクトを右クリックし「Build」を選択する。
+
+- HelloWorld
+	- out
+		- rom.bin
+
+をエミュレータで実行する。
---
+++ HelloWorld/src/main.c
@@ -0,0 +1,10 @@
+#include <genesis.h>
+
+int main()
+{
+    VDP_drawText("Hello, MegaDrive World!", 8, 13);
+
+    while (1) {}
+
+    return (0);
+}
